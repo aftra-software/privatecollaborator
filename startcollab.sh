62d5d392bc4f3effac0a84ac4491683d939b7ecd
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2024-04-24 11:27:56 +0000

    Add logging

--- startcollab.sh
+++ startcollab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/BurpSuitePro/BurpSuitePro -Xmx200m --collaborator-server --collaborator-config=/usr/local/collaborator/collaborator.config
+/usr/local/BurpSuitePro/BurpSuitePro -Xmx200m --collaborator-server --collaborator-config=/usr/local/collaborator/collaborator.config >> /var/log/collaborator.log
